1115bf277ebbb6e5b76491ccfb9eb6c4665dda9a
Chely Ho <adept_inept@yahoo.com>
2018-10-04 22:14:01 +0000

    automatically backed up by learn

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -9,7 +9,7 @@ class RestaurantsContainer extends Component {
     return (
       <div>
         <RestaurantInput addRestaurant={this.props.addRestaurant} />
-        <Restaurants restaurants={this.props.restaurants}/>
+        <Restaurants restaurants={this.props.restaurants} />
       </div>
     )
   }
