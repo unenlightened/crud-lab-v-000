c2b3ca4704c9b657e28a647526d916f8bb603d30
Chely Ho <adept_inept@yahoo.com>
2018-10-04 22:17:40 +0000

    automatically backed up by learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -3,7 +3,7 @@ import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
 
-  const restaurants = props => props.restaurants.map(restaurant => <Restaurant restaurant={restaurant} />)
+  const restaurants = props => props.restaurants.map(restaurant => <Restaurant restaurant={restaurant}/>)
 
   render() {
     return(
